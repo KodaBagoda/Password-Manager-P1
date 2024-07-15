7a9459da3395f9c83732da692115e46d90648eeb
0Day-EH <104030655+Unattributed@users.noreply.github.com>
2024-07-15 01:47:24 +0000

    added key file | added the 'add' function

--- pass_man.py
+++ pass_man.py
@@ -1,11 +1,33 @@
+from cryptography.fernet import Fernet
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+    return Fernet.generate_key()
+'''
+
+def load_key():
+    file = open("key.key", "rb").read()
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
 master_pwd = input("What is the master password: ")
+
+
+
+
 
 def view():
     with open("password_manager.txt", "r") as f:
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            
+            print(f"User: {user}, Password: {passw}")
+
 
 
 def add():
