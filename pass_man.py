e70338bbfcabc505b015ff05a4d1f3e623f25898
0Day-EH <104030655+Unattributed@users.noreply.github.com>
2024-07-16 18:09:16 -0700

    Update pass_man.py
     The master password is only used to derive the key and is not stored in memory.

--- pass_man.py
+++ pass_man.py
@@ -1,53 +1,56 @@
+import os
+import base64
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 from cryptography.fernet import Fernet
 
 '''
-def write_key():
-    key = Fernet.generate_key()
-    with open("key.key", "wb") as key_file:
-        key_file.write(key)
-    return Fernet.generate_key()
-
-write_key()
+# Function to load the encryption key
+def load_key():
+    with open("key.key", "rb") as key_file:
+        return key_file.read()
 '''
 
-def load_key():
-    file = open("key.key", "rb").read()
-    key = file.read()
-    file.close()
-    return key
+# Function to derive a key from the master password
+def derive_key(master_pwd, salt):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return base64.urlsafe_b64encode(kdf.derive(master_pwd.encode()))
 
+# Get the master password and create the Fernet object
 master_pwd = input("What is the master password: ")
-key = load_key() + master_pwd.bytes
+salt = load_key()  # Assuming the salt is stored in the key file
+key = derive_key(master_pwd, salt)
 fer = Fernet(key)
 
-
-
-
+# Function to view stored passwords
 def view():
     with open("password_manager.txt", "r") as f:
-        for line in f.readlines():
-            data = line.rstrip()
-            user, passw = data.split("|")
-            print(f"User: {user}, Password: {str(fer.decrypt(passw.encode()))}")
+        for line in f:
+            user, passw = line.strip().split("|")
+            print(f"User: {user}, Password: {fer.decrypt(passw.encode()).decode()}")
 
-
-
+# Function to add a new password entry
 def add():
     name = input("Enter the name of the entry: ")
     pwd = input("Enter the password for the entry: ")
-
     with open("password_manager.txt", "a") as f:
-        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
+        f.write(f"{name}|{fer.encrypt(pwd.encode()).decode()}\n")
 
+# Main loop to handle user input
 while True:
     mode = input("Would you like to add a new entry or retrieve an existing entry (view, add), press q to quit?: ").lower()
     if mode == "q":
         break
-
-    if mode == "view":
+    elif mode == "view":
         view()
     elif mode == "add":
         add()
     else:
         print("Invalid mode.")
-        continue 
